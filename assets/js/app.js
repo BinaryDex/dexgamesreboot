f8b06c706ee615401f97a61e3536383d5cc60ba7
Dex <82985536+BinaryDex@users.noreply.github.com>
2023-03-03 17:59:04 -0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,15 +2,15 @@
 function searchGame() {
   // DOM
   const input = document.getElementById('filter').value.toUpperCase();
-  const gameContainer = document.getElementById('product__container grid');
+  const gameContainer = document.getElementById('game-lists');
   console.log(gameContainer);
   
-  const games = gameContainer.getElementsByClassName('product__card');
+  const games = gameContainer.getElementsByClassName('game');
   console.log(games);
   
   
   for(let i = 0; i < games.length; i++) {
-    let title = games[i].querySelector(".product__body h3.product__title");
+    let title = games[i].querySelector(".game-body h3.game-title");
     console.log(title);
     
     if(title.innerText.toUpperCase().indexOf(input) > -1) {
