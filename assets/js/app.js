4c9effed4e0af961d5d2e9ea80da8fc957ced39a
Dex <82985536+BinaryDex@users.noreply.github.com>
2023-03-03 17:12:16 -0500

    Create app.js

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+function searchGame() {
+  // DOM
+  const input == document.getElementById('filter').value.toUpperCase()
+  ;
+  const gameContainer = document.getElementById('product__container grid');
+  console.log(gameContainer);
+  
+  const games = gameContainer.getElementByClassName('product__card');
+  console.log(games);
+  
+  
+  for(let i = 0; i < games.length; i++) {
+    let title = games[i].querySelector(".product__body h3.product__title");
+    console.log(title);
+    
+    if(title.innerText.toUpperCase().indexOf(input) > -1) {
+      games[i].style.display = "";
+    } else {
+      cards[i].style.display = "none";
+    }
+  }
+}
