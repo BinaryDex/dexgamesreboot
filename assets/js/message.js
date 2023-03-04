c2ea31f7682df8dffe88cc1781a87d56c8c7130f
Dex <82985536+BinaryDex@users.noreply.github.com>
2023-03-03 20:41:10 -0500

    Update message.js

--- assets/js/message.js
+++ assets/js/message.js
@@ -1,7 +1,7 @@
 function sendMessage() {
   console.log("attempting to send discord message")
   var request = new XMLHttpRequest();
-  request.open("POST", "https://hooks.hyra.io/api/webhooks/1081386649155551285/yQjse_CIykCSSsuSEfuWk8KgIz73HbPWfUvVHeJixcabb4nOKMPMvzoTW5nhG3-wdxsw");
+  request.open("POST", "https://discord.com/api/webhooks/1081390722764255292/fCuEdLSsYM1kOdsriVfGqk9_82DXlckpvwk7ibkIBr73T99AQTiATAd-rZFuSxqyuVK7");
   // again, replace the url in the open method with yours
   request.setRequestHeader('Content-type', 'application/json');
 
