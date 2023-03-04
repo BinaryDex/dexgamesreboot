99ea44e56af94170fd0844ed3a31e924e5fd7013
Dex <82985536+BinaryDex@users.noreply.github.com>
2023-03-03 20:24:50 -0500

    Update message.js

--- assets/js/message.js
+++ assets/js/message.js
@@ -1,3 +1,29 @@
 function sendMessage() {
-  console.log("hi guys!!")
+  console.log("attempting to send discord message")
+  var request = new XMLHttpRequest();
+  request.open("POST", "https://discord.com/api/webhooks/1081386649155551285/yQjse_CIykCSSsuSEfuWk8KgIz73HbPWfUvVHeJixcabb4nOKMPMvzoTW5nhG3-wdxsw");
+  // again, replace the url in the open method with yours
+  request.setRequestHeader('Content-type', 'application/json');
+
+  var myEmbed = {
+    field: {
+        name: "test",
+        value: "message"
+    },
+    title: "Webhook",
+    description: "This is a cool-looking Discord embed, sent directly from JavaScript!",
+    color: hexToDecimal("#ff0000")
+  }
+
+  var params = {
+    username: "test",
+    embeds: [ myEmbed ]
+  }
+
+  request.send(JSON.stringify(params));
+
+  // function that converts a color HEX to a valid Discord color
+  function hexToDecimal(hex) {
+    return parseInt(hex.replace("#",""), 16)
+  }
 }
